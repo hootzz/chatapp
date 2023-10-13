97dc9c647cd6dbb97d1dc7b8a0ae2ab58b080c4f
hootzz <wjqwjql3@gmail.com>
2023-10-13 18:56:17 +0900

    1013 chatApp

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const dotenv = require("express");
-const chats = require("./data/data");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
-dotenv.config
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send("API 돌아가는 중... ㅋㅋ nodemon을 사용하면 수정하고 다시 껐다가 켜지 않아도 됨");
@@ -18,5 +18,7 @@ app.get("/api/chat/:id", (req, res) => {
     res.send(singleChat);
 })
 
-app.listen(5000, console.log(`Server Started on PORT 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
  //5000 - whatever
